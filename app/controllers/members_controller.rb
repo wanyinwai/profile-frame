3be26992f2a86504e225fc635c3475b307f604d4
yinwai92 <wanyw92@gmail.com>
2015-07-25 06:25:48 +0800

    added session variable

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@ class MembersController < ApplicationController
   skip_before_filter :verify_authenticity_token
   before_action :set_member, only: [:show, :edit, :update, :destroy]
 
-  @temp_customer_id = ""
+  session[:current_member_id] = ""
 
   # get member info from ajax
   def memberinfo
@@ -16,7 +16,7 @@ class MembersController < ApplicationController
         # if member already been created, just redirect to index and render him out
         puts "came in exist"
 
-        @temp_customer_id = params[:customer_id]
+        session[:current_member_id] = params[:customer_id]
 
         redirect_to :action => "index", :customer_id => params[:customer_id] and return
       else
@@ -24,7 +24,7 @@ class MembersController < ApplicationController
         puts "came in not exist"
         @member = Member.create(:member_id =>params[:customer_id], :email => params[:customer_email])
 
-        @temp_customer_id = params[:customer_id]
+        session[:current_member_id] = params[:customer_id]
 
         redirect_to :action => "index", :customer_id => params[:customer_id] and return
       end
@@ -41,12 +41,12 @@ class MembersController < ApplicationController
   # GET /members.json
   def index
     puts "come in index"
-    puts @temp_customer_id
+    puts session[:current_member_id]
     # receive param from memberinfo redirect, then render user
     customer_id = params[:customer_id]
     if customer_id.blank?
 
-      @members = Member.where(:member_id => @temp_customer_id)
+      @members = Member.where(:member_id => session[:current_member_id])
       # potential hint here, when customer id is blank, cannot show anything
       puts "*****come in blank"
       #@members = Member.all
