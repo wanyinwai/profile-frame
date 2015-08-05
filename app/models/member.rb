4fd448c4457bcb2682899977a94047c5f02d9e23
yinwai92 <wanyw92@gmail.com>
2015-08-05 08:59:01 +0800

    changed paperclip path

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,11 @@
 class Member < ActiveRecord::Base
   # paperclip upload setting
 
-
 	has_attached_file :profilepic,
 	                :styles => { :medium => "300x300>", :thumb => "100x100>" },
 									:storage => :s3,
 									:default_url => "default.png",
-									:path => "profilecustom/twenty3/:member_id/:style/:filename",
+									:path => "profilecustom/twenty3/:member_id.:extension",
 									:bucket => ENV['AWS_BUCKET'],
 									:s3_host_name => "s3-ap-southeast-1.amazonaws.com",
 	                :s3_credentials => {
